15df7b331ed87635d61e9ef00c200dccbb948342
Divyesh Chitroda <div5yesh@gmail.com>
2020-06-21 22:15:45 -0400

    bugfix: generator model

--- rtl_fmri_gan.py
+++ rtl_fmri_gan.py
@@ -14,7 +14,7 @@ parser.add_argument('-m', '--model', default='model')
 parser.add_argument('-p', '--participant', default=1, type=int)
 parser.add_argument('-g', '--gpu', default='0')
 parser.add_argument('-d', '--delta', default=0.24, type=float)
-args = parser.parse_args(['-p','1','-m','model','-g','0'])
+args = parser.parse_args()
 print(args)
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
@@ -142,8 +142,8 @@ def generate_real_samples(dataset, n_samples):
 	X, labels = images[ix], labels[ix]
 	# generate class labels
 	# y = ones((n_samples, 1))
-	y = randint(7, 12, (n_samples, 1)) / 10
-	return [X, labels], y
+	# y = randint(7, 12, (n_samples, 1)) / 10
+	return X, labels#], y
 
 # generate points in latent space as input for the generator
 def generate_latent_points(latent_dim, n_samples, n_classes=60):
@@ -153,19 +153,20 @@ def generate_latent_points(latent_dim, n_samples, n_classes=60):
 	# z_input = x_input.reshape(n_samples, latent_dim)
 	z_input = tf.random.normal((n_samples, latent_dim), mean=0.0, stddev=1.0, dtype=tf.dtypes.float32)
 	# generate labels
-	labels = randint(0, n_classes, n_samples)
-	return [z_input, labels]
+	z_labels = randint(0, n_classes, n_samples)
+	return z_input, z_labels
  
 # use the generator to generate n fake examples, with class labels
-def generate_fake_samples(generator, latent_dim, n_samples):
+def generate_fake_samples(generator, latent_dim, n_samples, labels_input):
 	# generate points in latent space
-	z_input, labels_input = generate_latent_points(latent_dim, n_samples)
+	# z_input, labels_input = generate_latent_points(latent_dim, n_samples)
+	z_input, z_labels = generate_latent_points(latent_dim, n_samples)
 	# predict outputs
 	images = generator.predict([z_input, labels_input])
 	# create class labels
 	# y = zeros((n_samples, 1))
-	y = randint(0, 3, (n_samples, 1)) / 10
-	return [images, labels_input], y
+	# y = randint(0, 3, (n_samples, 1)) / 10
+	return images #, labels_input], y
 
 def prepare_images(vecs, voxel_map):
 	images = []
@@ -217,6 +218,10 @@ latent_dim = 1000
 dataset = [trainX, Y]
 optimizer = Adam(lr=0.0002, beta_1=0.5)
 
+def difference(inputs):
+	x1, x2 = inputs
+	return (x1 - x2)
+
 #%%
 input_shape = (51, 61, 23, 1)
 input_a = Input(shape=input_shape)
@@ -224,46 +229,36 @@ input_b = Input(shape=input_shape)
 in_label_a = Input(shape=(1,))
 in_label_b = Input(shape=(1,))
 
-gen_z_input = Input(shape=(latent_dim,))
-gen_label_input = Input(shape=(1,))
-
 d_model = define_discriminator(embeddings)
 g_model = define_generator(embeddings, latent_dim)
-
-# d1 = d_model([input_a, in_label_a])
-# d2 = d_model([input_b, in_label_b])
-
-def difference(inputs):
-	x1, x2 = inputs
-	return (x1 - x2)
 
 # construct dis model
 dis_d1 = d_model([input_a, in_label_a])
 dis_d2 = d_model([input_b, in_label_b])
 
-print(d_model, dis_d1)
-
 dis_dif_layer = Lambda(difference)([dis_d1, dis_d2])
 dis_out_layer = Dense(1, activation='sigmoid')(dis_dif_layer)
 
-dis_model = Model([input_a, input_b, in_label_a, in_label_b], outputs=dis_out_layer)
+dis_model = Model([input_a, input_b, in_label_a, in_label_b], outputs=dis_out_layer, name="dis_model")
 print(dis_model.summary())
 dis_model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
 # construct gen model
-# gen_g  = g_model([gen_z_input, gen_label_input])
+input_c = Input(shape=input_shape)
+in_label_c = Input(shape=(1,))
 gen_z_input, gen_label_input = g_model.input
-gen_d1 = d_model([input_a, in_label_a])
-gen_d1.trainable = False
+d_model.trainable = False
+gen_d1 = d_model([input_c, in_label_c])
+# gen_d1.trainable = False
 gen_d2 = d_model([g_model.output, gen_label_input])
-gen_d2.trainable = False
+# gen_d2.trainable = False
 
 gen_dif_layer = Lambda(difference)([gen_d1, gen_d2])
 gen_out_layer = Dense(1, activation='sigmoid')(gen_dif_layer)
 
-gen_model = Model([gen_z_input, input_a, gen_label_input, in_label_a], outputs=gen_out_layer)
+gen_model = Model([input_c, gen_z_input, in_label_c, gen_label_input], outputs=[gen_out_layer, g_model.output], name="gan_model")
 print(gen_model.summary())
-gen_model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+gen_model.compile(loss=['binary_crossentropy', perceptual_loss], optimizer=optimizer, metrics=['accuracy'])
 
 # %%
 def train(g_model, dis_model, gen_model, dataset, latent_dim, n_epochs=100, n_batch=2):
@@ -273,21 +268,25 @@ def train(g_model, dis_model, gen_model, dataset, latent_dim, n_epochs=100, n_ba
 	for i in range(n_epochs):
 		# enumerate batches over the training set
 		for j in range(bat_per_epo):
-			[X_real, labels_real], y_real = generate_real_samples(dataset, n_batch)
-			[X_fake, labels], y_fake = generate_fake_samples(g_model, latent_dim, n_batch)
+			y_real = randint(7, 12, (n_batch, 1)) / 10
+			X_real, labels_real = generate_real_samples(dataset, n_batch)
+			X_fake = generate_fake_samples(g_model, latent_dim, n_batch, labels_real)
 
-			d_loss, _ = dis_model.train_on_batch([X_real, X_fake, labels_real, labels], y_real)
+			d_loss, _ = dis_model.train_on_batch([X_real, X_fake, labels_real, labels_real], y_real)
 
-			[z_input, labels_input] = generate_latent_points(latent_dim, n_batch)
-			# y_gan = randint(7, 12, (n_batch, 1)) / 10
+			z_input, z_labels = generate_latent_points(latent_dim, n_batch)
+			y_fake = randint(0, 3, (n_batch, 1)) / 10
 
-			g_loss = gen_model.train_on_batch([z_input, labels_input, X_real, labels_real], y_fake)
+			g_loss, _ = gen_model.train_on_batch([dataset[0][z_labels], z_input, z_labels, z_labels], [y_fake, dataset[0][labels]])
 			
-			print('>%d, %d/%d, d=%.3f, g=%.3f' % (i+1, j+1, bat_per_epo, d_loss, g_loss[0]))
+			print('>%d, %d/%d, d=%.3f, g=%.3f, %.3f' % (i+1, j+1, bat_per_epo, d_loss, g_loss[0], g_loss[1]))
 	# save the generator model
 	g_model.save(os.path.join('pretrained', args.model + '_p' + str(args.participant) + '.h5'))
 
 # %%
-train(g_model, dis_model, gen_model, dataset, latent_dim, 1, 2)
+train(g_model, dis_model, gen_model, dataset, latent_dim, args.epoch, args.batch)
 
 #%%
+# from tensorflow.keras.utils import plot_model
+# plot_model(dis_model, to_file="dis.png", show_shapes=True)
+# plot_model(gen_model, to_file="gen.png", show_shapes=True)
